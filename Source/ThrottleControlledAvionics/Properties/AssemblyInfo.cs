17aefb738c47c165e32aeb6cdeb150b4352408ab
Lisias T <me@lisias.net>
2018-10-27 12:15:00 -0200

    Versioning Normalizing

--- Source/ThrottleControlledAvionics/Properties/AssemblyInfo.cs
+++ Source/ThrottleControlledAvionics/Properties/AssemblyInfo.cs
@@ -27,12 +27,12 @@ using AT_Utils;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-#if NIGHTBUILD
-[assembly: AssemblyVersion("3.5.*")]
-#else
-[assembly: AssemblyVersion("3.5.3.0")]
-#endif
-[assembly: KSPAssembly("ThrottleControlledAvionics", 3, 5)]
+[assembly: AssemblyVersion(ThrottleControlledAvionics.Version.Number)]
+[assembly: AssemblyFileVersion(ThrottleControlledAvionics.Version.Number)]
+[assembly: KSPAssembly("ThrottleControlledAvionics", ThrottleControlledAvionics.Version.major, ThrottleControlledAvionics.Version.major)]
+
+//[assembly: KSPAssemblyDependency("KSPe", 2, 0)]
+[assembly: KSPAssemblyDependency("AT_Utils", 1, 6)]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
@@ -40,18 +40,18 @@ using AT_Utils;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
-namespace ThrottleControlledAvionics
-{
-    public class ModInfo : KSP_AVC_Info
-    {
-        public ModInfo()
-        {
-            MinKSPVersion = new Version(1,4,5);
-            MaxKSPVersion = new Version(1,4,5);
+//namespace ThrottleControlledAvionics
+//{
+//    public class ModInfo : KSP_AVC_Info
+//    {
+//        public ModInfo()
+//        {
+//            MinKSPVersion = new Version(1,4,5);
+//            MaxKSPVersion = new Version(1,4,5);
 
-            VersionURL   = "https://raw.githubusercontent.com/allista/ThrottleControlledAvionics/master/GameData/ThrottleControlledAvionics/ThrottleControlledAvionics.version";
-            UpgradeURL   = "http://spacedock.info/mod/198/Throttle%20Controlled%20Avionics";
-            ChangeLogURL = "https://raw.githubusercontent.com/allista/ThrottleControlledAvionics/master/ChanegLog.md";
-        }
-    }
-}
+//            VersionURL   = "https://raw.githubusercontent.com/allista/ThrottleControlledAvionics/master/GameData/ThrottleControlledAvionics/ThrottleControlledAvionics.version";
+//            UpgradeURL   = "http://spacedock.info/mod/198/Throttle%20Controlled%20Avionics";
+//            ChangeLogURL = "https://raw.githubusercontent.com/allista/ThrottleControlledAvionics/master/ChanegLog.md";
+//        }
+//    }
+//}
